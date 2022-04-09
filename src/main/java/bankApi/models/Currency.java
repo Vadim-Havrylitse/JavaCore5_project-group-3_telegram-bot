f578ec2209a212f8799788e83c5d69e58fd60c60
Tetyana-Stepanova <stepanovatata05@gmail.com>
2022-04-09 22:19:01 +0300

    add PrivatApiService

--- src/main/java/bankApi/models/Currency.java
+++ src/main/java/bankApi/models/Currency.java
@@ -1,4 +1,10 @@
 package bankApi.models;
 
 public enum Currency {
+<<<<<<< b7da91b6e23c3f0697af277f0af83bfb3a69bfb5
+=======
+    USD,
+    EUR,
+    GBP
+>>>>>>> add PrivatApiService
 }
--- src/main/java/bankApi/service/BaseBankApiInterf.java
+++ src/main/java/bankApi/service/BaseBankApiInterf.java
@@ -1,6 +1,10 @@
 package bankApi.service;
 
 import bankApi.models.BaseBankResponse;
+<<<<<<< b7da91b6e23c3f0697af277f0af83bfb3a69bfb5
+=======
+import bankApi.models.CashCurrency;
+>>>>>>> add PrivatApiService
 import bankApi.models.Currency;
 
 import java.net.http.HttpClient;
@@ -9,5 +13,11 @@ public interface BaseBankApiInterf {
 
     HttpClient httpClient = HttpClient.newBuilder().build();
 
+<<<<<<< b7da91b6e23c3f0697af277f0af83bfb3a69bfb5
     BaseBankResponse getCurrency(Currency currency);
+=======
+    BaseBankResponse getBankCurrency(Currency currency);
+
+    CashCurrency getCurrentCurrency(Currency currency);
+>>>>>>> add PrivatApiService
 }
