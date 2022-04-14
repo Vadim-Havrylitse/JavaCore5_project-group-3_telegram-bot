dcdd14945b41379c1721e74aab76ae5f4d8d7918
Maver1k92 <bybjiik92@icloud.com>
2022-04-14 17:44:50 +0300

    Change BTC to GBR and add ISO code to monobank api

--- src/main/java/bankApi/models/Currency.java
+++ src/main/java/bankApi/models/Currency.java
@@ -1,7 +1,17 @@
 package bankApi.models;
 
-public enum Currency {
-    USD,
-    EUR,
-    BTC
+
+
+public enum Currency{
+
+    USD(840),
+    EUR(978),
+    GBP(826);
+    public final int codeISOL;
+    Currency(int codeISOL){
+        this.codeISOL = codeISOL;
+    }
+
+
+
 }
