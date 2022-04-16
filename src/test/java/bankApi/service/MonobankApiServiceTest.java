50f81405f5f9bdd9b327101b09f9da81b7f61cf0
Maver1k92 <bybjiik92@icloud.com>
2022-04-16 16:08:54 +0300

    Refactor

--- src/test/java/bankApi/service/MonobankApiServiceTest.java
+++ src/test/java/bankApi/service/MonobankApiServiceTest.java
@@ -17,13 +17,13 @@ public class MonobankApiServiceTest {
     public void getBankCurrency(){
         List<MononankResponse> monobankBankResponseList = monobankApiService.getBankCurrency();
         assertFalse(monobankBankResponseList.isEmpty());
-        assertEquals(980, monobankBankResponseList.get(0).getCurrencyCodeA());
+        assertEquals(840, monobankBankResponseList.get(0).getCurrencyCodeA());
         monobankBankResponseList.forEach(System.out::println);
     }
 
     @Test
     public void getCurrentCurrency(){
-        CashCurrency cashCurrency = monobankApiService.getCurrentCurrency(Currency.EUR);
+        CashCurrency cashCurrency = monobankApiService.getCurrentCurrency(Currency. EUR);
         assertNotNull(cashCurrency);
         assertNotEquals(0.0, cashCurrency.getValueBuy());
         assertNotEquals(0.0, cashCurrency.getValueSale());
